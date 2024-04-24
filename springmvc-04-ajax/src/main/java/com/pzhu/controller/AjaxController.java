244cf21718075a75b5c61620ca3fe4e966245703
faust6312 <153928430+faust6312@users.noreply.github.com>
2024-04-24 21:56:32 +0800

    Add files via upload

---
+++ springmvc-04-ajax/src/main/java/com/pzhu/controller/AjaxController.java
@@ -0,0 +1,62 @@
+package com.pzhu.controller;
+
+import com.pzhu.pojo.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class AjaxController {
+	@RequestMapping("t1")
+
+public String test(){
+return "hello!";
+
+}
+	@RequestMapping("/a1")
+	public void ajax1(String name , HttpServletResponse response) throws IOException {
+		if ("admin".equals(name)){
+			response.getWriter().print("true");
+		}else{
+			response.getWriter().print("false");
+		}
+	}
+
+
+
+
+	@RequestMapping("/a2")
+	public List<User> ajax2(){
+List<User> list = new ArrayList<User>();
+list.add(new User("JOE1号",3,"男"));
+list.add(new User("JOE2号",3,"男"));
+list.add(new User("JOE3号",3,"男"));
+return list; //由于@RestController注解，将list转成json格式返回
+	}
+
+
+	@RequestMapping("/a3")
+	public String ajax3(String name,String pwd){
+String msg = "";
+//模拟数据库中存在数据
+if (name!=null){
+ if ("admin".equals(name)){
+ msg = "OK";
+}else {
+ msg = "用户名输入错误";
+}
+ }
+ if (pwd!=null){
+  if ("123456".equals(pwd)){
+  msg = "OK";
+ }else {
+msg = "密码输入有误";
+ }
+ }
+return msg; //由于@RestController注解，将msg转成json格式返回
+	}
+}
